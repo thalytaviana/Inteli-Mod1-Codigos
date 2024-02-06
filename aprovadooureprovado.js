7a323b403a98b4eb4015d64309d21d70c2d8bfbd
thalytaviana <158761742+thalytaviana@users.noreply.github.com>
2024-02-06 15:53:30 -0300

    Add files via upload

---
+++ aprovadooureprovado.js
@@ -0,0 +1,12 @@
+var nota1 = 6
+var nota2 = 5
+var soma = (nota1+nota2)
+var media =  (soma/2)
+if (media >= 6){
+    console.log ("Sua média foi de", media)
+    console.log ("Você foi aprovado! :)")
+}
+else {
+    console.log ("Sua média foi de", media)
+    console.log ("Você foi reprovado :(")
+}
