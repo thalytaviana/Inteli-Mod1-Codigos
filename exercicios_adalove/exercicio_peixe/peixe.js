28be3c0fe434f34c6ba5f09b6f09b4cf24aaa553
thalytaviana <thalyta.viana@sou.inteli.edu.br>
2024-02-09 09:09:14 -0300

    exercicios

--- exercicios_adalove/exercicio_peixe/peixe.js
+++ exercicios_adalove/exercicio_peixe/peixe.js
@@ -14,14 +14,18 @@ var game = new Phaser.Game (config);
 var peixinho;
 
 function preload(){
-    this.load.image ('mar', 'assets/bg_azul-claro.png');
+    this.load.image ('mar', 'assets/fundo.png');
     this.load.image ('logo', 'assets/logo-inteli_branco.png');
     this.load.image ('peixe', 'assets/peixe_rosa.png');
+    this.load.image ('abacaxi', 'assets/abacaxi_bob.png');
+    this.load.image ('bob', 'assets/bob.png');
 }
 
 function create (){
-    this.add.image (400,300, 'mar');
+    this.add.image (400,300, 'mar').setScale (1.5);
+    this.add.image (400,400, 'abacaxi').setScale(2.0);
     this.add.image (400, 525, 'logo').setScale(0.5);
+    this.add.image (650, 490, 'bob').setScale (0.5);
 
     peixinho = this.add.image (400,300, 'peixe');
     peixinho.setFlip (true,false);
