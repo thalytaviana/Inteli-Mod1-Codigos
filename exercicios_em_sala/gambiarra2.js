0408c06f37581acb8fe73f82167eda4c08e37f58
thalytaviana <thalyta.viana@sou.inteli.edu.br>
2024-02-20 18:42:11 -0300

    Update gambiarra2.js

--- exercicios_em_sala/gambiarra2.js
+++ exercicios_em_sala/gambiarra2.js
@@ -1,30 +1,31 @@
 var altura = 1.88; // Altura em metros
 var peso = 103;    // Peso em quilogramas
-var imc;
-var resultado;
 
-function verificarIMC(imc){
-    imc = peso / (altura * altura);
+function verificarIMC(){
+    var imc = peso / (altura * altura);
     imc = imc.toFixed(2); // Arredondar o resultado para duas casas decimais
 
     switch (true){
-        case imc >= 25 && imc < 29.9:
-            console.log ("Sobrepeso");
+        case imc < 18.5:
+            console.log("Você está abaixo do peso");
             break;
         case imc >= 18.5 && imc < 24.9:
-            console.log ("Peso normal");
+            console.log("Você está no peso normal");
+            break;
+        case imc >= 25 && imc < 29.9:
+            console.log("Você está no sobrepeso");
             break;
         case imc >= 30 && imc < 34.9:
-            console.log ("Obesidade grau 1");
-            break;
-        case imc < 18.5:
-            console.log ("Abaixo do peso");
+            console.log("Você está na obesidade grau 1");
             break;
         case imc >= 35 && imc < 39.9:
-            console.log ("Obesidade grau 2")
+            console.log("Você está na obesidade grau 2");
             break;
         default:
-            console.log ("Obesidade grau 3");
+            console.log("Você está na obesidade grau 3");
     }
-console.log("IMC:", imc);
+    return imc; // você pode retornar o valor do IMC se desejar
 }
+
+var imc = verificarIMC();
+console.log ("IMC:", imc);
